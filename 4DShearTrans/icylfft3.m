9abd1dcaea93e2bbc2fbcafd382075ea98015dd6
tommheik <heik.tommi@gmail.com>
2021-09-01 13:01:04 +0300

    Cleaned (again)

--- 4DShearTrans/icylfft3.m
+++
@@ -1,5 +0,0 @@
-function f = icylfft3(F)
-%ICYLFFT3 is a cylindrical iFFT performed only on the first 3 dimensions
-%   T H   2021
-f = real(ifft(ifft(ifft(F,[],1),[],2),[],3));
-end
--- BP/DoPyrRec.m
+++
@@ -1,4 +0,0 @@
-function xrec=DoPyrRec(L)
-Pyr_mode=1;%1.5;
-smooth_func = @rcos;
-xrec = PyrNDRec_mm(L, 'S', Pyr_mode, smooth_func);
